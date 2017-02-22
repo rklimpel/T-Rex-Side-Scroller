cd0f84c84aeef9a9b7eb01b9b1b02324017033fa
Rico Klimpel <rico@tklimpel.de>
2017-02-23 00:35:15 +0100

    train with javaFx

--- T-Rex Sidescroller Projekt/src/programming/projekt/trex/Controller.java
+++ T-Rex Sidescroller Projekt/src/programming/projekt/trex/Controller.java
@@ -1,4 +1,22 @@
 package programming.projekt.trex;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+
+
+public class Controller{
+
+    public Button btn_startDesktop;
+
+    public void onClick_btn_startDeskop(){
+
+        if(btn_startDesktop.getText().equals("hallo")){
+            btn_startDesktop.setText("Start Game Deksop");
+        }else{
+            btn_startDesktop.setText("hallo");
+        }
+
+    }
+
 }
