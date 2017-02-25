707ce432b850749f9525858ed0723819d8912525
JanekBalster <janek-balster@freenet.de>
2017-02-25 20:30:13 +0100

    levelloading implemented

--- T-Rex Sidescroller Projekt/src/programming/projekt/trex/GameModel.java
+++ T-Rex Sidescroller Projekt/src/programming/projekt/trex/GameModel.java
@@ -7,7 +7,13 @@ import java.util.TimerTask;
 /**
  * Created by ricoklimpel on 23.02.17.
  */
-public class GameModel{
+//array for the levelstructure
+public class GameModel {
+    int[] lvl = {10, 300, 500, 10, 10, 800, 10, 700};
+
+    int counterLvlArray = 0;
+    int obstacleTick = 0;
+
 
     //Counts up how often the game timer has ticked,
     //can be used to generate the complete gametime
@@ -49,7 +55,7 @@ public class GameModel{
      *
      * @param gameController
      */
-    public GameModel(GameController gameController){
+    public GameModel(GameController gameController) {
         this.gameController = gameController;
     }
 
@@ -67,16 +73,16 @@ public class GameModel{
     /**
      * Creates a new Player instace from Player class
      */
-    public void createPlayer(){
+    public void createPlayer() {
         getPane();
-        player = new Player(paneWidth,paneHeight);
+        player = new Player(paneWidth, paneHeight);
     }
 
     /**
      * Called by Space, makes the player jump
      */
-    public void jump(){
-        if(!player.isJumping){
+    public void jump() {
+        if (!player.isJumping) {
             player.jump();
         }
     }
@@ -84,15 +90,15 @@ public class GameModel{
     /**
      * Creates a new Obstacle instance from Obstacle class
      */
-    public void createObstacle(){
-        obstacle = new Obstacle(paneWidth,paneHeight);
+    public void createObstacle() {
+        obstacle = new Obstacle(paneWidth, paneHeight);
         obstacles.add(obstacle);
     }
 
     /**
      * Starts the continous ticking Game Timer
      */
-    public void startGameTimer(){
+    public void startGameTimer() {
         TimerTask task = new TimerTask() {
             public void run() {
                 GameTick();
@@ -106,7 +112,7 @@ public class GameModel{
     /**
      * Stops the continous ticking Game Timer
      */
-    public void stopGameTimer(){
+    public void stopGameTimer() {
         gameTimer.cancel();
         gameTimer.purge();
         gameTimerEnabled = false;
@@ -115,17 +121,19 @@ public class GameModel{
     /**
      * Fires on every GameTimer tick
      * object and other movements should be done here
-     *
+     * <p>
      * sends update notification to controller
      */
-    public void GameTick(){
+    public void GameTick() {
 
         timerTick += 1;
+        checklvl();
+
 
         for (int i = 0; i < obstacles.size(); i++) {
-            if(!obstacles.get(i).checkOutisde()){
+            if (!obstacles.get(i).checkOutisde()) {
                 obstacles.get(i).moveLeft();
-            }else{
+            } else {
                 obstacles.remove(i);
                 System.out.println("removed obstacle");
             }
@@ -134,4 +142,34 @@ public class GameModel{
         //Notify the Gamecontroller about value changes
         gameController.Update();
     }
+
+    /**
+     * reads the lvl-Array and creates the objects on the
+     * specific spots.
+     */
+    private void checklvl() {
+
+
+        if (obstacles != null && obstacleTick == lvl[counterLvlArray]/*+  obstacles.get(obstacles.size()-1).getWidth()*/) {
+            createObstacle();
+
+
+            counterLvlArray += 1;
+            obstacleTick = 0;
+
+        }
+        if (obstacles == null && obstacleTick == lvl[counterLvlArray]) {
+            createObstacle();
+
+
+            counterLvlArray += 1;
+            obstacleTick = 0;
+        }
+        if (counterLvlArray == lvl.length - 1) {
+            counterLvlArray = 0;
+        }
+        obstacleTick += 1;
+    }
+
+
 }
