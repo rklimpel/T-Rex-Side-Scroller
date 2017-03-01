dca5996c6e12ab5926eb34caad80b87e35bc3025
Rico Klimpel <rico@tklimpel.de>
2017-03-01 23:39:56 +0100

    comments & better Game - view/controller communication

--- T-Rex Sidescroller Projekt/src/main/java/cau/project/game/controller/GameController.java
+++ T-Rex Sidescroller Projekt/src/main/java/cau/project/game/controller/GameController.java
@@ -4,6 +4,8 @@ import main.java.cau.project.Helper;
 import main.java.cau.project.Main;
 import main.java.cau.project.View;
 import main.java.cau.project.game.model.GameModel;
+import main.java.cau.project.game.model.Obstacle;
+import main.java.cau.project.game.model.Player;
 
 import java.util.ArrayList;
 
@@ -13,22 +15,50 @@ public class GameController {
 
     public GameModel gameModel;
 
+    /**
+     * Constructor adds calling view to Listener List and creates a new GameModel with default Size
+     * if no gameModle instanz exists
+     *
+     * @param newListener
+     */
     public GameController(View newListener) {
         this.listeningViews.add(newListener);
-        this.gameModel = new GameModel(this,800,400);
+        if(this.gameModel==null){
+            this.gameModel = new GameModel(this,800,400);
+        }
     }
 
+    /**
+     *
+     * Constructor adds calling view to listener List and creates a new GameModel with views requierments
+     * if no gameModel Instanz exists
+     *
+     * @param newListener
+     * @param paneWidth
+     * @param paneHeight
+     */
     public GameController(View newListener,int paneWidth, int paneHeight){
         this.listeningViews.add(newListener);
-        this.gameModel = new GameModel(this,paneWidth,paneHeight);
+        if(this.gameModel==null){
+            this.gameModel = new GameModel(this,paneWidth,paneHeight);
+        }
     }
 
+    /**
+     * Update, Called by gamemodel on gametick
+     * updates all listening views
+     */
     public void update() {
         for (int i = 0; i < listeningViews.size(); i++) {
             listeningViews.get(i).Update();
         }
     }
 
+    /**
+     * one of the views called jump
+     *
+     * @param view
+     */
     public void jump(View view) {
 
         //IF Game is not over but it is not Running start the game
@@ -48,14 +78,29 @@ public class GameController {
         }
     }
 
+    /**
+     * one of the views called crouch
+     */
     public void crouch() {
         gameModel.player.crouch();
     }
 
+    /**
+     * one of the views called crouch end
+     */
     public void crouchEnd() {
         gameModel.player.crouchEnd();
     }
 
+    /**
+     * one of the views called quit game
+     * set Score to Helper variable to access it from EndView
+     * if there is no listening view left determinate game Controller and stop game models gameTimer
+     *
+     * send exit request to the view that called quitGame
+     *
+     * @param view
+     */
     public void quitGame(View view) {
         listeningViews.remove(view);
         if (listeningViews.size() == 0) {
@@ -64,5 +109,32 @@ public class GameController {
         }
         Helper.score = gameModel.getScore();
         view.exit();
+    }
+
+    /**
+     * Gets Obstacles from Gamemodel
+     *
+     * @return
+     */
+    public ArrayList<Obstacle> getObstacles(){
+        return gameModel.getObstacles();
+    }
+
+    /**
+     * Gets player from gamemodel
+     *
+     * @return
+     */
+    public Player getPlayer(){
+        return gameModel.getPlayer();
+    }
+
+    /**
+     * Gets Score from gamemodel
+     *
+     * @return
+     */
+    public int getScore(){
+        return gameModel.getScore();
     }
 }
--- T-Rex Sidescroller Projekt/src/main/java/cau/project/game/view/desktop/DesktopView.java
+++ T-Rex Sidescroller Projekt/src/main/java/cau/project/game/view/desktop/DesktopView.java
@@ -30,24 +30,32 @@ public class DesktopView extends View {
    @FXML
    BorderPane background;
 
-   private Label lbl_hints = new Label("Press Space to Jump \n Press Strg to crouch ");
+   private Label lbl_hints = new Label();
 
    private Rectangle rectangle;
 
-   private ImageView imageView_player = new ImageView();
-   private ImageView imageView_obstacle;
+   private ImageView iV_player = new ImageView();
+   private ImageView iv_Obstacle;
 
    private ImageLoader imageLoader;
    private CustomFontLoader customFontLoader;
 
    private Font customFont;
 
-   private GameController gameController;
+   private GameController controller;
 
    private Boolean gameObjectsAsImages = R.gameobjectsAsImages;
 
-   private Boolean hintsShown = false;
-
+   /**
+    * Deskotp View Constructor
+    *
+    * set a view variable to use if "this" is not possible
+    * sets the View ID
+    * set Keyboard and Mouse Listeners
+    * Load Images and Fonts
+    * Show Hints
+    *
+    */
    public DesktopView() {
 
       view = this;
@@ -58,8 +66,8 @@ public class DesktopView extends View {
          public void run() {
 
             if (Main.gameController == null) {
-               Main.gameController = new GameController(view,(int)pane.getWidth(),(int)pane.getHeight());
-               gameController = Main.gameController;
+               Main.gameController = new GameController(view, (int) pane.getWidth(), (int) pane.getHeight());
+               controller = Main.gameController;
             }
 
             new KeyboardListener(view);
@@ -77,17 +85,17 @@ public class DesktopView extends View {
    }
 
    /**
-    * Leave Game, quit every running things and go back to Menu
+    * Button back to Menu's onClick
     *
-    * @throws IOException
+    * calls Controllers quit game
     */
    public void OnClick_btn_backToMenu() {
-      gameController.quitGame(this);
+      controller.quitGame(this);
    }
 
    /**
     * Is called on GameModel Data changes
-    * e.g. in gametick method
+    * Creates a new "Frame" / Scene and creates all game components new
     */
    public void Update() {
 
@@ -118,9 +126,12 @@ public class DesktopView extends View {
             drawGround();
          }
       });
-
    }
 
+   /**
+    * draw a single, not moving ground line over the whole pane width (at groundlvl)
+    * add this ground rectangle to pane
+    */
    private void drawGround() {
 
       Rectangle rectangle = new Rectangle();
@@ -131,62 +142,80 @@ public class DesktopView extends View {
       pane.getChildren().addAll(rectangle);
    }
 
+   /**
+    * create a new Image with obstacle values from gamecontroller -> gamemodell
+    * add obstacle Image to pane
+    *
+    * will be called for every obstacle in obstacle array list
+    */
    private void drawImageObstacles() {
 
+      for (int i = 0; i < controller.gameModel.getObstacles().size(); i++) {
 
-      for (int i = 0; i < gameController.gameModel.getObstacles().size(); i++) {
+         iv_Obstacle = new ImageView();
+         iv_Obstacle.setImage(imageLoader.getImg_obstacle());
+         iv_Obstacle.relocate(controller.getObstacles().get(i).getX(),
+                 controller.getObstacles().get(i).getY());
+         iv_Obstacle.setFitHeight(controller.getObstacles().get(i).getHeight());
+         iv_Obstacle.setFitWidth(controller.getObstacles().get(i).getWidth());
 
-         imageView_obstacle = new ImageView();
-         imageView_obstacle.setImage(imageLoader.getImg_obstacle());
-         imageView_obstacle.relocate(gameController.gameModel.getObstacles().get(i).getX(),
-                 gameController.gameModel.getObstacles().get(i).getY());
-         imageView_obstacle.setFitHeight(gameController.gameModel.getObstacles().get(i).getHeight());
-         imageView_obstacle.setFitWidth(gameController.gameModel.getObstacles().get(i).getWidth());
-
-         pane.getChildren().addAll(imageView_obstacle);
+         pane.getChildren().addAll(iv_Obstacle);
       }
-
-
    }
 
-   private void drawImagePlayer() {
-
-      if (gameController.gameModel.player.getCrouching()) {
-         imageView_player.setImage(imageLoader.getImg_playerCrouched());
-      } else {
-         imageView_player.setImage(imageLoader.getImg_player());
-      }
-      imageView_player.setFitWidth(gameController.gameModel.player.getWidth());
-      imageView_player.setFitHeight(gameController.gameModel.player.getHeight());
-      imageView_player.relocate(gameController.gameModel.player.getX(), gameController.gameModel.player.getY());
-
-      pane.getChildren().addAll(imageView_player);
-
-   }
-
+   /**
+    * create a new rectangle with obstacle values from gamecontroller -> gamemodell
+    * add obstacle rectangle to pane
+    *
+    * will be called for every obstacle in obstacle array list
+    */
    private void drawObstacles() {
 
-      for (int i = 0; i < gameController.gameModel.getObstacles().size(); i++) {
+      for (int i = 0; i < controller.gameModel.getObstacles().size(); i++) {
 
          rectangle = new Rectangle(
-                 gameController.gameModel.getObstacles().get(i).getX(),
-                 gameController.gameModel.getObstacles().get(i).getY(),
-                 gameController.gameModel.getObstacles().get(i).getWidth(),
-                 gameController.gameModel.getObstacles().get(i).getHeight());
+                 controller.getObstacles().get(i).getX(),
+                 controller.getObstacles().get(i).getY(),
+                 controller.getObstacles().get(i).getWidth(),
+                 controller.getObstacles().get(i).getHeight());
 
          pane.getChildren().addAll(rectangle);
       }
    }
 
-   private void drawPlayer() {
-      rectangle = new Rectangle(
-              gameController.gameModel.player.getX(),
-              gameController.gameModel.player.getY(),
-              gameController.gameModel.player.getWidth(),
-              gameController.gameModel.player.getHeight());
+   /**
+    * create a new Image with the players values from gamecontroller -> gamemodel
+    * add player Image to pane
+    */
+   private void drawImagePlayer() {
 
-      rectangle.setRotate(gameController.gameModel.player.getRotation());
-      rectangle.setFill(gameController.gameModel.player.getColor());
+      if (controller.gameModel.player.getCrouching()) {
+         iV_player.setImage(imageLoader.getImg_playerCrouched());
+      } else {
+         iV_player.setImage(imageLoader.getImg_player());
+      }
+      iV_player.setFitWidth(controller.getPlayer().getWidth());
+      iV_player.setFitHeight(controller.getPlayer().getHeight());
+      iV_player.relocate(controller.getPlayer().getX(), controller.getPlayer().getY());
+
+      pane.getChildren().addAll(iV_player);
+
+   }
+
+   /**
+    * create a new rectangle with the players values from gamecontroller -> gamemodel
+    * add player rectangle to pane
+    */
+   private void drawPlayer() {
+
+      rectangle = new Rectangle(
+              controller.getPlayer().getX(),
+              controller.getPlayer().getY(),
+              controller.getPlayer().getWidth(),
+              controller.getPlayer().getHeight());
+
+      rectangle.setRotate(controller.getPlayer().getRotation());
+      rectangle.setFill(controller.getPlayer().getColor());
       rectangle.setStroke(Color.BLACK);
       rectangle.setStrokeWidth(2);
       rectangle.setStrokeLineCap(StrokeLineCap.ROUND);
@@ -195,10 +224,15 @@ public class DesktopView extends View {
 
    }
 
+   /**
+    * Create a new Label, set Style and Position on Pane
+    * get Score from GameController -> gamemodel and wirte score inside it
+    * add this score label to pane
+    */
    private void drawScoreLabel() {
 
       Label label = new Label();
-      label.setText("Score: " + gameController.gameModel.getScore());
+      label.setText("Score: " + controller.getScore());
       label.setFont(customFont);
 
       FontLoader fontLoader = Toolkit.getToolkit().getFontLoader();
@@ -210,26 +244,32 @@ public class DesktopView extends View {
       pane.getChildren().add(label);
    }
 
+   /**
+    * Exit Desktop Game Screen when getting exit call from Game Controller
+    *
+    * check for Gameover, if not exit to menu
+    */
    public void exit() {
-      if(gameController.gameModel.gameOver){
+      if (controller.gameModel.gameOver) {
          SceneSwitcher.END.load();
-      }else{
+      } else {
          SceneSwitcher.MENU.load();
       }
    }
 
    /**
-    * shows hints and creates them
+    * Show Hints how to move the player with the Keyboard to the User
+    *
+    * will automatically be destroyed on first update
     */
    private void showHints() {
-
-      hintsShown = true;
 
       Platform.runLater(new Runnable() {
          @Override
          public void run() {
 
-            lbl_hints.setFont(customFontLoader.load(R.fontPixel,80));
+            lbl_hints.setText("Press Space to Jump \n Press Strg to crouch ");
+            lbl_hints.setFont(customFontLoader.load(R.fontPixel, 80));
 
             FontLoader fontLoader = Toolkit.getToolkit().getFontLoader();
             float btn_hintsWidth = fontLoader.computeStringWidth(lbl_hints.getText(), lbl_hints.getFont());
@@ -240,7 +280,5 @@ public class DesktopView extends View {
             pane.getChildren().addAll(lbl_hints);
          }
       });
-
    }
-
 }
