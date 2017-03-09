2294f32114b8fe3b3746eb5386baf1704aec274a
Rico Klimpel <rico@tklimpel.de>
2017-03-09 15:41:32 +0100

    removed Janek only solution

--- T-Rex Sidescroller Projekt/src/main/java/cau/project/screens/game/controller/GameController.java
+++ T-Rex Sidescroller Projekt/src/main/java/cau/project/screens/game/controller/GameController.java
@@ -62,7 +62,6 @@ public class GameController {
          }
       };
       updateTimer.scheduleAtFixedRate(task, 0, 8);
-      //for janek laptop the period must be between 18 and 20
 
       nextWalkingImg = 12;
    }
